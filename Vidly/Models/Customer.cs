d0375d1be871c1f377c060dbe8905733a9c53c66
lolocro <bosniclovro@gmail.com>
2019-12-09 17:00:24 +0100

    initial commit (after chapter 5)

---
+++ Vidly/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer name.")]
+        [StringLength(225)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "date")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+    }
+}
